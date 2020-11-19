5bc8f1fe7adfcd64346e14c605ceab177acbe734
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-11-19 18:56:23 +0700

    mencari gradien

---
+++ src/pboif2/pkg10119043/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119043.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author FERMI
+ * NAMA      : FERMI NAUFAL AKBAR
+ * NIM       : 10119043
+ * KELAS     : IF2
+ * DESKRIPSI : Program menghitung gradien
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
